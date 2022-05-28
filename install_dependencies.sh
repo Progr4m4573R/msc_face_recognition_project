1d8e18756acd0f76d249a5d612b9b44dbce95dad
Stephen Rerri-Bekibele <ghostye@hotmail.com>
2022-05-29 00:41:04 +0100

    Modified install script for Face recognition project to make installation of dependencies much easier. Added sudo su to script to enable running of the python_setup.py file

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -30,4 +30,5 @@ cd ~ && \
     mkdir -p dlib && \
     git clone -b 'v19.9' --single-branch https://github.com/davisking/dlib.git dlib/ && \
     cd  dlib/ && \
+    sudo su ~ \
     python3 setup.py install --yes USE_AVX_INSTRUCTIONS
